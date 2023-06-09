f632fae7998bc48d62352a8e601c01bfba5d5223
Himanshu Verma <himanshuv242cp@gmail.com>
2023-06-09 23:37:11 +0530

    Soil moisture level api integrated

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { ScrollView, StyleSheet, TouchableOpacity,Image,Alert, } from 'react-native';
 import {Block, Text} from '../components/Dashboard/Index';
 import * as theme from '../constants/Dashboard/theme';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import SoundPlayer from 'react-native-sound-player';
-import { useEffect } from 'react';
 import { fetchWeatherForecast } from '../../api/weather';
 import { getData} from '../../utils/asyncStorage';
  
@@ -51,6 +50,7 @@ const Dashboard = ({ navigation }) => {
   const [isMotorOn, setIsMotorOn] = useState('');
   const [isLightOn, setIsLightOn] = useState('');
   const [weather, setWeather] = useState({});
+  const [moistureLevel, setMoistureLevel] = useState('');
   const {current } = weather;
 
   //fetch current city weather data
@@ -70,6 +70,33 @@ const Dashboard = ({ navigation }) => {
   };
   useEffect(() => {
     fetchMyWeatherData();
+  }, []);
+
+  // for fetching moisture level every .5 second
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+        const savedIPAddress = await AsyncStorage.getItem('ipAddress');
+        await axios.get(`http://${savedIPAddress}/soilsensor`).then(response => {
+          // console.log(response.data);
+          const stringifiedData = JSON.stringify(response.data);
+          const value= Math.round(((1024-stringifiedData)/1024)*100);
+
+          setMoistureLevel(value);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+
+      } catch (error) {
+        console.error('Failed to send API request', error);
+      }
+    };
+
+    const interval = setInterval(fetchData, 500); // Fetch data every .5 seconds
+    // console.log(waterLevel);
+    return () => clearInterval(interval); // Cleanup interval on component unmount
   }, []);
   
 //API call for switching motor
@@ -155,6 +182,9 @@ const Dashboard = ({ navigation }) => {
   };
 
 
+
+
+
   return (
       <Block style={styles.dashboard}>
          <Block column style={{ marginTop: theme.sizes.base }}>
@@ -175,7 +205,7 @@ const Dashboard = ({ navigation }) => {
           
           <Block flex={1} column style={{alignItems:'center'}}>
             <Text caption >Soil Moisture</Text>
-            <Text size={40} height={80} color={'#0AC4BA'}>91%</Text>
+            <Text size={40} height={80} color={'#0AC4BA'}>{moistureLevel}%</Text>
           </Block>
         </Block> 
 
